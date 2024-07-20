fbc9aa8e708ba5fa7e8e5ed2c62f34893a574ad4
swati28624 <swati_vinod@navgurukul.org>
2024-07-20 10:26:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import FormComponent from './FormComponent'; // Adjust the path based on your file structure
+
+function App() {
+  return (
+    <div className="App">
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
+
+
